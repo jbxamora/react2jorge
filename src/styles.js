2b03a376421a724748972a0c080a1c8dae841dd9
jbxamora <jbxamora@icloud.com>
2023-03-14 23:16:18 -0700

    created navbar component

--- src/styles.js
+++ src/styles.js
@@ -1,4 +1,4 @@
-const styles = {
+const Styles = {
     paddingX: "sm:px-16 px-6",
     paddingY: "sm:py-16 py-6",
     padding: "sm:px-16 px-6 sm:py-16 py-10",
@@ -14,4 +14,4 @@ const styles = {
         "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
 };
 
-export { styles };
+export { Styles };
