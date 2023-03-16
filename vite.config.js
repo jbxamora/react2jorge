37efc9417d726088688f995f897b30c7956bef03
jbxamora <jbxamora@icloud.com>
2023-03-15 23:32:01 -0700

    pushing final build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build',
+    emptyOutDir: true
+  },
+  base: '/react2jorge/'
+});
+
