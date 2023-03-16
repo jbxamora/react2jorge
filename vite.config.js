4a3eafbf78ef4d065cdc30fbb82e835ec0aa4d75
jbxamora <jbxamora@icloud.com>
2023-03-16 00:22:11 -0700

    troubleshooting deployment

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build',
-    emptyOutDir: true
-  },
-  base: '/react2jorge/'
-});
-
+})
